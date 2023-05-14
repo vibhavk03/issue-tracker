1008bd015b5a003ef66043158956ed2826728468
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-14 13:33:23 +0530

    adding mongo db connection

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/issue_tracker_local');
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function () {
+  console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = 3000;
 
-// const db = require('./config/mongoose.js');
+const db = require('./config/mongoose.js');
 
 const app = express();
 
