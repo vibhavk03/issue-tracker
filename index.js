1541166e1cd44f454bcd52d2e4cdfd3218349e63
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-14 11:27:47 +0530

    adding prettierrc file for single quotes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const port = 3000;
 
 // const db = require('./config/mongoose.js');
@@ -7,19 +7,19 @@ const port = 3000;
 const app = express();
 
 // including static assets
-app.use(express.static("./assets"));
+app.use(express.static('./assets'));
 
 // setting app engine as ejs
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 // refrencing views folder
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
 // home page
-app.get("/", function (req, res) {
-  res.render("home", {
-    test: "yay express connected to ejs",
+app.get('/', function (req, res) {
+  res.render('home', {
+    test: 'yay express connected to ejs',
   });
 });
 
