7b23041822cb25c8104e5b605666b04e167efb41
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-14 11:23:37 +0530

    configuring express and adding home view

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+// const db = require('./config/mongoose.js');
+
+const app = express();
+
+// including static assets
+app.use(express.static("./assets"));
+
+// setting app engine as ejs
+app.set("view engine", "ejs");
+// refrencing views folder
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+// home page
+app.get("/", function (req, res) {
+  res.render("home", {
+    test: "yay express connected to ejs",
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error in starting server! => error: ${err}`);
+    return;
+  }
+  console.log(`Server listening on port: ${port}`);
+});
