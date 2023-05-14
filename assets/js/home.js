7496359208c3ccac22494f7182a9a7524ce8109d
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-14 16:59:22 +0530

    adding new project input fields and styling modal

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,11 +2,18 @@ const createProjectBtn = document.getElementById('create-project-btn');
 const modalContainer = document.getElementById('modal-container');
 const closeModalBtn = document.getElementById('create-project-close-modal-btn');
 
+const projectTitleInput = document.getElementById('project-title-input');
+const projectDescriptionInput = document.getElementById(
+  'project-description-input'
+);
+
 createProjectBtn.addEventListener('click', () => {
-  console.log('12345678');
   modalContainer.classList.add('show-modal');
 });
 
 closeModalBtn.addEventListener('click', () => {
   modalContainer.classList.remove('show-modal');
+  /* removea any left over values from modal */
+  projectTitleInput.value = '';
+  projectDescriptionInput.value = '';
 });
