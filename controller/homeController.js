b85dab03f7fb3a0830df7b56c165130c1d6c4c78
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-15 20:12:42 +0530

    rendering projects fetched from database

--- controller/homeController.js
+++ controller/homeController.js
@@ -2,7 +2,7 @@ const Project = require('../models/project');
 
 module.exports = {
   home: async function (req, res) {
-    const projects = await Project.find({});
+    const projects = await Project.find({}).sort('-createdAt');
     console.log(projects);
     res.render('home', {
       projects,
