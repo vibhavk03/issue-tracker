2ff27391cab5db1d230a8bf519daa2f9572216af
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-15 20:33:21 +0530

    adding delete project button on project display

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const projectController = require('../controller/projectController');
 
 router.post('/create', projectController.create);
+// router.delete('/:id', projectController.deleteProject);
 
 module.exports = router;
