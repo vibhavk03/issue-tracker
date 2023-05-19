583fee55e9fa26263d427259a610be63f7a34f5f
vibhavk03 <vibhavkashyap3@gmail.com>
2023-05-19 22:26:23 +0530

    handling create issue controller

--- controller/issueController.js
+++ controller/issueController.js
@@ -1,6 +1,30 @@
+const Project = require('../models/project');
+const Issue = require('../models/issue');
+
 module.exports = {
-  create: (req, res) => {
-    console.log('create an issue here');
+  create: async (req, res) => {
+    const {
+      'issue-title': title,
+      'issue-description': description,
+      'issue-author': author,
+      'checkbox-labels': labels,
+    } = req.body;
+    try {
+      const issue = await Issue.create({
+        title,
+        description,
+        author,
+        labels,
+        project: req.params.id,
+      });
+      // console.log('Issue created!', issue);
+      const project = await Project.findById(req.params.id);
+      project.issues.push(issue);
+      await project.save();
+      // console.log('Project saved!', project);
+    } catch (error) {
+      console.log(`Error in creating an issue : ${error}`);
+    }
     res.redirect('back');
   },
 };
