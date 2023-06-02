a6ac05205083d05c41c540f373b7ade2f5f7df94
vibhavk03 <vibhavkashyap3@gmail.com>
2023-06-02 22:22:59 +0530

    trimming project schema values

--- models/project.js
+++ models/project.js
@@ -4,14 +4,17 @@ const projectSchema = new mongoose.Schema(
   {
     title: {
       type: String,
+      trim: true,
       required: true,
     },
     description: {
       type: String,
+      trim: true,
       required: true,
     },
     author: {
       type: String,
+      trim: true,
       required: true,
     },
     issues: [
